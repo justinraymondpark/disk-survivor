e1fc0389323e35a064f1859276693c55f815fee0
Justin Raymond Park <justinraymondpark@Mac.fios-router.home>
2025-08-13 00:51:54 -0400

    fix: add startRX/endRX to altInsertAnim to satisfy TS type; adjust chooseAltFloppy insert anim accordingly

--- src/game.ts
+++ src/game.ts
@@ -3560,7 +3560,7 @@ class Game {
     const sel = this.altFloppies[0].mesh
     const start = sel.position.clone()
     const end = new THREE.Vector3(0, 2.5, 0.3)
-	this.altInsertAnim = { m: sel, t: 0, dur: 450, start, end, startR: sel.rotation.y, endR: 0, onDone: () => {
+	this.altInsertAnim = { m: sel, t: 0, dur: 450, start, end, startR: sel.rotation.y, endR: 0, startRX: sel.rotation.x, endRX: 0, onDone: () => {
 		if (this.altTitleGroup) this.scene.remove(this.altTitleGroup)
 		this.altTitleGroup = undefined
 		this.altTitleActive = false
